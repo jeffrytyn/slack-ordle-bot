a2ba96f39e8e06c4b317134517eacd45dc7b6a10
Jeffrey Tian <jeffreytian@ist-15292.apn.wlan.private.upenn.edu>
2023-01-25 15:25:14 -0500

    show og user command

--- functions/ordle-add.js
+++ functions/ordle-add.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, setDoc, doc } from "firebase/firestore";
-
+import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 import qs from "qs";
 import {   
@@ -44,8 +44,16 @@ export async function handler({body}, context){
   const body_obj = qs.parse(body) || {};
   const user_id = body_obj.user_id;
   const text = body_obj.text?.toLowerCase() || "";
+  fetch(body_obj.response_url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        response_type: "in_channel",
+        text: body_obj.command + " " + body_obj.text
+      })
+    });
   const [game_id, score] = parse_app_mention(text);
-  let game_ref;
+  let game_ref; let game;
   if(game_id === -1){
     return {
       statusCode: 200,
@@ -56,13 +64,17 @@ export async function handler({body}, context){
       })}
   }
   if(game_id === 0){
-    game_ref = doc(db, "wordle", user_id);
+    game = "wordle"
+    game_ref = doc(db, game, user_id);
   }else if(game_id === 1){
-    game_ref = doc(db, "worldle", user_id);
+    game = "worldle"
+    game_ref = doc(db, game, user_id);
   }else if(game_id === 2){
-    game_ref = doc(db, "quordle", user_id);
+    game = "quordle"
+    game_ref = doc(db, game, user_id);
   }else if(game_id === 3){
-    game_ref = doc(db, "countryle", user_id);
+    game = "countryle"
+    game_ref = doc(db, game, user_id);
   }
   setDoc(game_ref, {
     score: score
@@ -71,7 +83,7 @@ export async function handler({body}, context){
     statusCode: 200,
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
-      response_type: "ephemeral",
-      text: "Your score of " + score + " has been recorded."
+      response_type: "in_channel",
+      text: `Your score of ${score} for ${game} has been recorded.`
     })}
 };
