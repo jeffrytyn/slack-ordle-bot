4d43c621260e5169a199c6a32f79ba9dfaba3983
Jeffrey Tian <jeffreytian@Jeffreys-MacBook-Pro.local>
2023-04-20 22:23:12 -0500

    stat

--- functions/ordle-stat.js
+++ functions/ordle-stat.js
@@ -36,36 +36,22 @@ export async function handler({body, headers}, context){
       return {game_title, month_total: snap.exists() ? snap.data().total : 0};
     }
     ));
-    // const promises = [getDocs(collection(db, game, user_id, "scores")), getDoc(doc(db, game, user_id))];
-    // const settled = await Promise.allSettled(promises);
-    // let days = 0;
-    // let total = 0;
-    // let month_total = 0;
-    // if(settled[0].status === "fulfilled"){
-    //   days = settled[0].value.docs.length;
-    //   total = settled[0].value.docs.reduce((acc, doc) => acc + doc.data().score, 0);
-    // }
-    // if(settled[1].status === "fulfilled" && settled[1].value.exists()){
-    //   month_total = settled[1].value.data().total;
-    // }
-    // stats.push(`*${game_title} stats:*\n\
-    // Lifetime days played: ${days}\n\
-    // Lifetime average score: ${days === 0 ? 'N/A': (total/days).toFixed(2)}\n\
-    // Total score this month: ${month_total}`);
   }
   const settled = await Promise.allSettled(promises);
   settled.forEach(promise => {
     if(promise.status === "fulfilled"){
       const {game_title, days=0, total=0, month_total=0} = promise.value;
-      if(days) stats[game_title].days = days;
-      if(total) stats[game_title].total = total;
-      if(month_total) stats[game_title].month_total = month_total;
+      stats[game_title].days = days;
+      stats[game_title].total = total;
+      stats[game_title].month_total = month_total;
+    }else{
+      console.log(promise.reason);
     }
   });
   const text = Object.entries(stats).map(([game, stats]) => `*${game} stats:*\n\
-  Lifetime days played: ${stats.days}\n\
-  Lifetime average score: ${stats.days === 0 ? 'N/A': (stats.total/stats.days).toFixed(2)}\n\
-  Total score this month: ${stats.month_total}`).join("\n");
+    Lifetime days played: ${stats.days}\n\
+    Lifetime average score: ${stats.days === 0 ? '_N/A_': (stats.total/stats.days).toFixed(2)}\n\
+    Total score this month: ${stats.month_total}`).join("\n");
 
   return {
     statusCode: 200,
